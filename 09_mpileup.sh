0443c6e3175afd352e81b3332891004b0e35820d
Gabriele <g.nocchi@hotmail.com>
2025-02-25 21:58:52 +0100

    Update 09_mpileup.sh

--- 09_mpileup.sh
+++ 09_mpileup.sh
@@ -10,7 +10,7 @@
 
 module load bcftools/1.11
 
-CHROM=$(sed -n "${SLURM_ARRAY_TASK_ID}p" chromosomes.txt) ### list of chromosomes. This species has 14, that s why array number is 14. We parallelize by crhomosome
+CHROM=$(sed -n "${SLURM_ARRAY_TASK_ID}p" chromosomes.txt) ### list of chromosomes (can be found in the FASTA index file of reference genome -- .fai file). This species has 14, that s why array number is 14. We parallelize by crhomosome
 
 
 ### here we call SNPs. list.txt is a list of ALL the realigned bam files of ALL samples. if you have multiple bam per samples due to multiple libraries, merge them into one before this step. ploidymap is a list that keeps the same order of samples in list.txt and should use the sample ID names you set in script 5, and for each
