70c48cfacafbade5b73d4555988926b4ffe8b70a
Gabriele <g.nocchi@hotmail.com>
2025-02-25 22:39:36 +0100

    Update 09_mpileup.sh

--- 09_mpileup.sh
+++ 09_mpileup.sh
@@ -10,7 +10,7 @@
 
 module load bcftools/1.11
 
-CHROM=$(sed -n "${SLURM_ARRAY_TASK_ID}p" chromosomes.txt) ### list of chromosomes (can be found in the FASTA index file of reference genome -- .fai file). This species has 14, that s why array number is 14. We parallelize by crhomosome
+CHROM=$(sed -n "${SLURM_ARRAY_TASK_ID}p" chromosomes.txt) ### list of chromosomes (can be found in the FASTA index file of reference genome -- .fai file). This species has 14, that s why array number is 14. We parallelize by crhomosome. if you had a very fragmented reference with like 1000s of scaffolds, rather than sending 1000 jobs, you can feed a list of chromosomes to the command. so you can for example split 1000 scaffolds into 10 lists of 100 scaffolds names each, and feed those 10 lists to the command using a job array of size 10 (1-10)
 
 
 ### here we call SNPs. list.txt is a list of ALL the realigned bam files of ALL samples. If you have multiple bam per samples due to multiple libraries, merge them into one before this step. ploidymap is a list that keeps the same order of samples in list.txt and should use the sample ID names you set in script 05, and for each
