32383c7015fb4a2b9e2a564e176af1012fd356b6
Gabriele <g.nocchi@hotmail.com>
2025-02-25 22:15:26 +0100

    Update 05_add_RG.sh

--- 05_add_RG.sh
+++ 05_add_RG.sh
@@ -5,13 +5,13 @@
 #SBATCH --account=def-yeaman
 #SBATCH --array=1-83
 
-### Keep the lists below with the same order of samples
+### Keep the lists below with the same order
 INPUT=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list1.txt) ### List of input deduplicated bam files
 OUTPUT=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list2.txt) ### List of output names (just remove .bam) from the inputs
 NAME=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list3.txt)   ### Here you want to extract the sample name from the input name, which is used to set the read IDs. Can be the same as list2
 
 
-### Here we add read groups, we start with our deduplicated bam files and we get a deduplicated bam with read groups assigned per sample
+### Here we add read groups, we start with our deduplicated bam files and we get a deduplicated bam with read groups assigned per sample/library
 module load picard java
 
 java -jar $EBROOTPICARD/picard.jar AddOrReplaceReadGroups I=$INPUT O=$OUTPUT\_RG.bam RGID=$NAME RGLB=$NAME\_LB RGPL=ILLUMINA RGPU=unit1 RGSM=$NAME
