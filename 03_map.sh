b29730cfdd37faaf82be9ffea35504f571121ce0
Gabriele <g.nocchi@hotmail.com>
2025-02-25 19:11:55 +0100

    Update 03_map.sh

--- 03_map.sh
+++ 03_map.sh
@@ -5,11 +5,14 @@
 #SBATCH --account=def-yeaman
 #SBATCH --array=1-83
 
-INPUT1=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list1.txt)
-INPUT2=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list2.txt)
-OUTPUT=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list3.txt)
+INPUT1=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list1.txt) ### List of trimmed R1 reads (produced with script 01)
+INPUT2=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list2.txt) ###  List of trimmed R2 reads (produced with script 01)
+OUTPUT=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list3.txt) ### List of output name -- just extract the meaningful part of the name from the reads names
 
 module load bwa samtools
+
+#### Here we map the trimmed reads to the ref genome and produce a sam, then we sort it, convert it to bam and we index it
+#### We end up with 1 bam per sample after this
 
 
 bwa mem -t 4 Betula_pendula_subsp._pendula.faa $INPUT1 $INPUT2  > ./bwa_output/$OUTPUT\.sam
