eed91467f87581857a8c40ba3d87e0a9d293574d
Gabriele <g.nocchi@hotmail.com>
2025-02-25 19:32:48 +0100

    Update 11_filter.sh

--- 11_filter.sh
+++ 11_filter.sh
@@ -8,13 +8,14 @@
 
 module load vcftools bcftools
 module load StdEnv/2020 intel/2020.1.217 tabix/0.2.6
+### Filter the final VCF produced in script 10-- no MAF filter
 
 bcftools filter -e 'MQ < 30' *vcf.gz -Oz > tmp.vcf.gz
 
-vcftools --gzvcf tmp.vcf.gz --max-missing 0.7 --minQ 30 --minGQ 20 --minDP 5 --max-alleles 2 --recode --recode-INFO-all --stdout > nocchi_bplaty_filtered.vcf
+vcftools --gzvcf tmp.vcf.gz --max-missing 0.7 --minQ 30 --minGQ 20 --minDP 5 --max-alleles 2 --recode --recode-INFO-all --stdout > bplaty_filtered.vcf
 
 rm tmp.vcf.gz
 
 
-bgzip nocchi_bplaty_filtered.vcf
-tabix -p vcf nocchi_bplaty_filtered.vcf.gz
+bgzip bplaty_filtered.vcf
+tabix -p vcf bplaty_filtered.vcf.gz
