c5794ec9e200f3ed93dbd8fae806e35fd44d2ce3
Gabriele <g.nocchi@hotmail.com>
2025-02-25 21:41:17 +0100

    Update 02_bwa_index.sh

--- 02_bwa_index.sh
+++ 02_bwa_index.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=def-yeaman
 
-### Here we just create bwa index of the reference genome, needed for mapping
+### Here we just create the bwa index of the reference genome, needed for mapping
 ### Keep the output of this command in the same dir where you keep the reference genome fasta
 
 module load bwa 
