5a82903fc4000ce0b176d0a7b41fde8d68f84fb3
Gabriele <g.nocchi@hotmail.com>
2025-02-25 22:17:11 +0100

    Update 07_realign_indel.sh

--- 07_realign_indel.sh
+++ 07_realign_indel.sh
@@ -6,8 +6,8 @@
 #SBATCH --array=1-83
 
 
-### Keep the lists below with the same order of samples
-INPUT=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list1.txt) ### list of bam files with read groups (output of 06)
+### Keep the lists below with the same order
+INPUT=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list1.txt) ### list of bam files with read groups (output of script 05)
 OUTPUT=$(sed -n "${SLURM_ARRAY_TASK_ID}p" list2.txt)  ### list of output names (just remove .bam suffix from input list)
 
 module load StdEnv/2020 samtools/1.12
@@ -19,7 +19,7 @@ module purge
 module load nixpkgs/16.09
 module load java gatk/3.8
 
-#### Here we generate a indel realigned bam file for each sample
+#### Here we generate a indel realigned bam file for each sample/library
 # Realign
 java -jar $EBROOTGATK/GenomeAnalysisTK.jar -T RealignerTargetCreator -R Betula_pendula_subsp._pendula.fa -I $INPUT -o $OUTPUT\.intervals
 
