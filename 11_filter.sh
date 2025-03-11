b0069decb03f9d086bc2313b18bd8bc21332bde8
Gabriele <g.nocchi@hotmail.com>
2025-03-11 10:24:16 +0100

    Update 11_filter.sh
    adeded filter for invariant alt/alt sites

--- 11_filter.sh
+++ 11_filter.sh
@@ -10,10 +10,10 @@ module load vcftools bcftools
 module load StdEnv/2020 intel/2020.1.217 tabix/0.2.6
 
 
-########### Only filtering the VCF to exclude sites with QUAL < 30 ###########
+########### Only filtering the VCF to exclude sites with QUAL < 30 and invariant ALT/ALT sites (AC = AN) ###########
 
 
-bcftools filter -e 'MQ < 30' bplaty.vcf.gz -Ov > bplaty_filtered.vcf
+bcftools filter -e 'AC=AN || MQ < 30' bplaty.vcf.gz -Ov > bplaty_filtered.vcf
 
 ### As an alternative to the bcftools command above, vcftools can also be used to filter by QUAL:
 ### vcftools --gzvcf bplaty.vcf.gz --minQ 30 --recode --recode-INFO-all --stdout > bplaty_filtered.vcf
